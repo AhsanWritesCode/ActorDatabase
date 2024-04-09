e3f9c3a66a324ec3d3def057eef137fca6b1a3e7
Ahsan <ahsan.tariq@ucalgary.ca>
2024-04-08 21:22:14 -0600

    program executes (updated MainController.java

--- src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
+++ src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
@@ -1,14 +1,90 @@
 package ca.ucalgary.ahsantariq.demogui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label actors_label;
+
+    @FXML
+    private TextField costar_id1;
+
+    @FXML
+    private TextField costar_id2;
+
+    @FXML
+    private Label costars_label;
+
+    @FXML
+    private Button info_id1;
+
+    @FXML
+    private TextField retire_id1;
+
+    @FXML
+    private TextField sep_id1;
+
+    @FXML
+    private TextField sep_id2;
+
+    @FXML
+    void about(ActionEvent event) {
+
     }
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getLoneActorsCount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void load(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuActorStatistics(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuPrintActorInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retire(ActionEvent event) {
+
+    }
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+    @FXML
+    void splitPair(ActionEvent event) {
+
+    }
+
+    @FXML
+    void storeNewActor(ActionEvent event) {
+
+    }
+
+    @FXML
+    void storeNewCoStarPair(ActionEvent event) {
+
+    }
+
 }
