b67e84709a349cda1eecc145e308913459e22c81
rahnuhanurain <rahnuha.nurain@ucalgary.ca>
2024-04-09 23:05:27 -0600

    Titles added for MainGUI.java

--- src/main/java/ca/ucalgary/ahsantariq/demogui/MainGUI.java
+++ src/main/java/ca/ucalgary/ahsantariq/demogui/MainGUI.java
@@ -12,7 +12,7 @@ public class MainGUI extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainGUI.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Actor tracker!");
         stage.setScene(scene);
         stage.show();
     }
