6262ee527ea47b68dfa10bb3c69025ad40f43d7f
rahnuhanurain <rahnuha.nurain@ucalgary.ca>
2024-04-09 23:44:02 -0600

    About method completed

--- src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
+++ src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
@@ -3,7 +3,9 @@ package ca.ucalgary.ahsantariq.demogui;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 
 public class MainController {
@@ -45,6 +47,21 @@ public class MainController {
 
     @FXML
     public void about(ActionEvent actionEvent) {
+        // Create an alert to display information about the application
+        Alert about = new Alert(Alert.AlertType.INFORMATION, "Author: Rahnuha Nurain,Ahsan Tariq, Ethan Braum  \n Email: rahnuha.nurain@ucalgary.ca,ahsan.tariq@ucalgary.ca,ethan.braum@ucalgary.ca nVersion: v1.0\nThis is" +
+                " retrieves and organizes actor information from IMDb links for effortless access and storage\"\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n" +
+                "\n", ButtonType.OK);
+        // Set the title of the alert
+        about.setTitle("About");
+        // Set the header text of the alert
+        about.setHeaderText("Message");
+        // Show the alert and wait for user interaction
+        about.showAndWait();
     }
 
     @FXML
