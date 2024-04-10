5dfa665a4e6ffd9dd56c29eb010e21d7b16a544e
rahnuhanurain <rahnuha.nurain@ucalgary.ca>
2024-04-09 23:11:09 -0600

    Adding Button fx:id's to MainController.java

--- src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
+++ src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
@@ -1,11 +1,14 @@
 package ca.ucalgary.ahsantariq.demogui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MainController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button createCoStarButton;
 
     @FXML
     protected void onHelloButtonClick() {
