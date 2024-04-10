5192b77c74c44e0b0f22888222be9cd936e878f0
Ahsan <ahsan.tariq@ucalgary.ca>
2024-04-09 23:49:33 -0600

    progressed on maincontroller

--- src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
+++ src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
@@ -8,6 +8,39 @@ import javafx.scene.control.TextField;
 
 public class MainController {
 
+    private static Data data = new Data();
+
+    // Format string for displaying actor information
+    private static final String ACTOR_FORMAT = "%-20s\t%-3s\t%-6s\t%-30s\t%-11s\t%-36s\t%-7s%n";
+    // Header for displaying actor information
+    private static final String ACTOR_HEADER = String.format(ACTOR_FORMAT, "NAME", "AGE", "HEIGHT", "COUNTRY", "# OF AWARDS", "IMDB LINK", "RETIRED");
+    // Separator string for formatting purposes
+    private static String ACTOR_SEPARATOR = "";
+
+    // Static block to initialize the separator string
+    static {
+        // Loop through each character in the actor header and concatenate '-' to form a separator string
+        for (int i = 0; i < ACTOR_HEADER.length(); i++) {
+            ACTOR_SEPARATOR += "-";
+        }
+    }
+
+    // Format string for displaying actor pairs
+    private static final String PAIR_FORMAT = "%-32s %-33s%n";
+    // Header for displaying actor pairs
+    private static final String PAIR_HEADER = String.format(PAIR_FORMAT, "IMDBLINK1", "IMDBLINK2");
+    // Separator string for formatting purposes
+
+    private static String PAIR_SEP = "";
+
+    // Static block to initialize the separator string
+    static {
+        // Loop through each character in the pair header and concatenate '-' to form a separator string
+        for (int i = 0; i < PAIR_HEADER.length(); i++) {
+            PAIR_SEP += "-";
+        }
+    }
+
     @FXML
     private Label actors_label;
 
