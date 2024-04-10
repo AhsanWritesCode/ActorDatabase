d062b2750a59849c980e142a1957f3e51ed206c3
rahnuhanurain <rahnuha.nurain@ucalgary.ca>
2024-04-09 23:29:11 -0600

    on action method for menuActorStatistics created

--- src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
+++ src/main/java/ca/ucalgary/ahsantariq/demogui/MainController.java
@@ -51,4 +51,8 @@ public class MainController {
     @FXML
     public void menuPrintActorInfo(ActionEvent actionEvent) {
     }
+
+    @FXML
+    public void menuActorStatistics(ActionEvent actionEvent) {
+    }
 }
